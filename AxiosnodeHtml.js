46088e3076f80ed8bd6a3038bead68d221f638c8
SIRAPHONG <seanta000@gmail.com>
2024-02-05 11:21:22 +0700

    6406021611077

--- AxiosnodeHtml.js
+++ AxiosnodeHtml.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const path = require("path")
 var bodyParser = require('body-parser');
 
 const base_url = "http://localhost:3000";
 
-//app.set("views",path.join(__dirname,"views"));
+app.set("views",path.join(__dirname,"views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "views"));
 
 app.get("/", async (req,res) => {
     try {
